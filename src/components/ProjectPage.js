410bf7c22ec634daf6229cb96a35de1714da6954
RYZENWALE <arya247aviero@gmail.com>
2024-06-30 11:01:34 +0530

    build error ahhhh

--- src/components/ProjectPage.js
+++ src/components/ProjectPage.js
@@ -2,36 +2,36 @@ import React from "react";
 import "./ProjectPage.css";
 import { HiMiniArrowLongRight } from "react-icons/hi2";
 
-const ProjectData = [
-  {
-    name: "Secret Santa",
-    desc: "Developed 'Secret Santa,' a Christmas-themed event creator app using RedwoodJS for the frontend, GraphQL for the API, and Supabase with PostgreSQL for the backend. Implemented full CRUD functionality, automated event reminders, and role-based access control to facilitate secure and joyous holiday gift exchanges.",
-    liveLink: "",
-    stack: ["RedwoodJS", "GraphQL", "Supabase"],
-    github: "https://github.com/Not-yourCoder/secretSanta.git",
-  },
-  {
-    name: "All Start Stats",
-    desc: "Currently developing a comprehensive web application to provide the latest football news, live scores, and detailed team and player statistics. Integrating real-time data fetching via Rapid API and creating interactive data visualizations with Chart.js. Gradually incorporating machine learning models to predict match outcomes and player performances.",
-    liveLink: "",
-    stack: ["React", "Tailwind", "Rest Api"],
-    github: "https://github.com/Not-yourCoder/all-star-stats.git",
-  },
-  {
-    name: "News Aggregator",
-    desc: "A simple website news aggregator built using Python and HTML. The aggregator scans any given website URL and displays news articles in an HTML page. It leverages web scraping techniques to extract relevant content and present it in a user-friendly format.",
-    liveLink: "",
-    stack: ["Html", "Css", "Python"],
-    github: "https://github.com/Not-yourCoder/news-aggregator",
-  },
-  {
-    name: "Space Singer",
-    desc: "Developed a working clone of Spotify, showcasing comprehensive frontend design and functionality. Implemented features include user authentication, browsing music library, creating playlists, and playing tracks with seamless audio streaming. Built using React.js, RESTful API integration for music data, and styled with tailwind for responsive design.",
-    liveLink: "",
-    stack: ["React.js", "REST API", "Tailwind Css"],
-    github: "https://github.com/Not-yourCoder/space.singer",
-  },
-];
+// const ProjectData = [
+//   {
+//     name: "Secret Santa",
+//     desc: "Developed 'Secret Santa,' a Christmas-themed event creator app using RedwoodJS for the frontend, GraphQL for the API, and Supabase with PostgreSQL for the backend. Implemented full CRUD functionality, automated event reminders, and role-based access control to facilitate secure and joyous holiday gift exchanges.",
+//     liveLink: "",
+//     stack: ["RedwoodJS", "GraphQL", "Supabase"],
+//     github: "https://github.com/Not-yourCoder/secretSanta.git",
+//   },
+//   {
+//     name: "All Start Stats",
+//     desc: "Currently developing a comprehensive web application to provide the latest football news, live scores, and detailed team and player statistics. Integrating real-time data fetching via Rapid API and creating interactive data visualizations with Chart.js. Gradually incorporating machine learning models to predict match outcomes and player performances.",
+//     liveLink: "",
+//     stack: ["React", "Tailwind", "Rest Api"],
+//     github: "https://github.com/Not-yourCoder/all-star-stats.git",
+//   },
+//   {
+//     name: "News Aggregator",
+//     desc: "A simple website news aggregator built using Python and HTML. The aggregator scans any given website URL and displays news articles in an HTML page. It leverages web scraping techniques to extract relevant content and present it in a user-friendly format.",
+//     liveLink: "",
+//     stack: ["Html", "Css", "Python"],
+//     github: "https://github.com/Not-yourCoder/news-aggregator",
+//   },
+//   {
+//     name: "Space Singer",
+//     desc: "Developed a working clone of Spotify, showcasing comprehensive frontend design and functionality. Implemented features include user authentication, browsing music library, creating playlists, and playing tracks with seamless audio streaming. Built using React.js, RESTful API integration for music data, and styled with tailwind for responsive design.",
+//     liveLink: "",
+//     stack: ["React.js", "REST API", "Tailwind Css"],
+//     github: "https://github.com/Not-yourCoder/space.singer",
+//   },
+// ];
 
 const ProjectPage = () => {
   return (
